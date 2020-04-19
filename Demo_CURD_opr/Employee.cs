08276ea34642b9fed6b4e343ba72db06937f80e1
Hamza442 <56518775+Hamza442@users.noreply.github.com>
2020-04-19 17:05:38 +0500

    Add files via upload

---
+++ Demo_CURD_opr/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLayer
+{//we deleted models folder vedio 11 for reference
+    //we added entity frame work to use Data annotations
+    public class Employee
+    {
+        public int  ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+    }
+
+}
---
+++ Demo_CURD_opr/EmployeeBusinessLayer.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;//this is added to use command type
+using System.Data.SqlClient;
+using System.Configuration;//we have added this to add configuration manager and also added asembly in the references .Video 11 7:32
+
+
+namespace BusinessLayer
+{//this file will contain all the businees logic
+    //in reality this layer is gonig to containthe the busines rules and validtions but we are onlu going to have some data acees code
+    //go to MVdemo right click on it add Refernces go to projects and add Bussiness Layer
+    public class EmployeeBusinessLayer
+    {
+        //the property Employees is going to return list of employees 
+        public IEnumerable<Employee> Employees
+        {
+            get
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                List<Employee> employees = new List<Employee>();
+                using(SqlConnection con = new SqlConnection(connectionString)){
+                    SqlCommand cmd = new SqlCommand("spGetAllEmployees",con);
+                    cmd.CommandType=CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while(rdr.Read()){
+                        Employee employee = new Employee();
+                        employee.ID=Convert.ToInt32(rdr["ID"]);
+                        employee.Name=rdr["Name"].ToString();
+                        employee.Gender=rdr["Gender"].ToString();
+                        employee.City=rdr["City"].ToString();
+                        employee.DateOfBirth=Convert.ToDateTime(rdr["DateOfBirth"]);
+
+                        employees.Add(employee);
+
+                    }
+                }
+                return employees;
+            }
+        }
+        //this is going to add data to database table 
+        public void AddEmployee(Employee employee)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spAddEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramName = new SqlParameter();
+                paramName.ParameterName = "@Name";
+                paramName.Value = employee.Name;
+                cmd.Parameters.Add(paramName);
+
+                SqlParameter paramGender = new SqlParameter();
+                paramGender.ParameterName = "@Gender";
+                paramGender.Value = employee.Gender;
+                cmd.Parameters.Add(paramGender);
+
+                SqlParameter paramCity = new SqlParameter();
+                paramCity.ParameterName = "@City";
+                paramCity.Value = employee.City;
+                cmd.Parameters.Add(paramCity);
+
+                SqlParameter paramDateOfBirth = new SqlParameter();
+                paramDateOfBirth.ParameterName = "@DateOfBirth";
+                paramDateOfBirth.Value = employee.DateOfBirth;
+                cmd.Parameters.Add(paramDateOfBirth);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                  
+            }
+        }
+        //this going to update data in data base table
+        public void SaveEmployee(Employee employee)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spSaveEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramId = new SqlParameter();
+                paramId.ParameterName = "@ID";
+                paramId.Value = employee.ID;
+                cmd.Parameters.Add(paramId);
+
+                SqlParameter paramName = new SqlParameter();
+                paramName.ParameterName = "@Name";
+                paramName.Value = employee.Name;
+                cmd.Parameters.Add(paramName);
+
+                SqlParameter paramGender = new SqlParameter();
+                paramGender.ParameterName = "@Gender";
+                paramGender.Value = employee.Gender;
+                cmd.Parameters.Add(paramGender);
+
+                SqlParameter paramCity = new SqlParameter();
+                paramCity.ParameterName = "@City";
+                paramCity.Value = employee.City;
+                cmd.Parameters.Add(paramCity);
+
+                SqlParameter paramDateOfBirth = new SqlParameter();
+                paramDateOfBirth.ParameterName = "@DateOfBirth";
+                paramDateOfBirth.Value = employee.DateOfBirth;
+                cmd.Parameters.Add(paramDateOfBirth);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+        //this is going to delete data from database 
+        public void DeleteEmployee(int id)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spDeleteEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+    
+                SqlParameter paramId = new SqlParameter();
+                paramId.ParameterName = "@ID";
+                paramId.Value = id;
+                cmd.Parameters.Add(paramId);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
